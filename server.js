5aeec69d62c4773e5f563164ee571b9334896f5e
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-09-07 04:11:21 -0700

    update path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 
@@ -12,34 +13,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-// const Role = db.role;
 
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
 db.sequelize.sync();
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Fangqi Yuan application." });
 });
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-// function initial() {
-//   Role.create({
-//     id: 1,
-//     name: "user"
-//   });
- 
-//   Role.create({
-//     id: 2,
-//     name: "admin"
-//   });
-// }
