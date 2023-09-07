352d072ff4afcceaf41478e660a0419e8f613a6a
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-09-07 00:15:15 -0700

    update db.sq

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "Yuan$990611",
-  DB: "testdb",
+  HOST: "dpg-cjsmgcm3m8ac73doojs0-a.oregon-postgres.render.com",
+  USER: "gamers_user",
+  PASSWORD: "769HrOyjYS6pAPx89bvwU65a6wY08Jra",
+  DB: "gamers",
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   pool: {
     max: 5,
     min: 0,
